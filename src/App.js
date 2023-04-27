06aa8aa20b9ce0af58bb4d1522576aa7d2641e69
uday chauhan <chauhanuday842@gmail.com>
2023-04-27 12:56:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [text, settext] = useState("")
+  const [item, setitem] = useState([])
+  const [searchitem, setsearchitem] = useState([])
+
+
+
+useEffect(()=>{
+  fetch('https://fakestoreapi.com/products')
+  .then((res)=>res.json())
+  .then((datas)=>{
+    setitem(datas)
+    setsearchitem(datas)
+
+  })
+},[])
+
+const searchfunc=(text)=>{
+  if(text===""){
+    setitem(searchitem)
+  }else{
+    let searchdata=item.filter((e)=>{
+      return e.title.toLowerCase().indexOf(text.toLowerCase()) > -1;
+    })
+    setitem(searchdata)
+  }
+      
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <h2>Search Products</h2>
+        <div>
+          <input type="text" placeholder='serach here...' value={ text } onChange={ (e) => {
+            searchfunc(e.target.value)
+            settext(e.target.value)
+          } } />
+        </div>
+      </div>
+      <div className='itemsdiv'>
+        {
+          item && item.map((ele) => {
+            return <>
+              <div class="card" style={{width: "18rem",height:"22rem" ,margin:"1rem"}}>
+                <img src={ele.image} class="card-img-top" alt="..."/>
+                  <div class="card-body">
+                    <h5 class="card-title">{ele.title}</h5>
+                    
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+              </div>
+            </>
+          })
+        }
+      </div>
     </div>
   );
 }
